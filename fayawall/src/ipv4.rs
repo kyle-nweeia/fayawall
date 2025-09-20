f784914d4218d19b22eaf3d2a970bc821e7c6677
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-09-20 16:36:07 -0600

    Test Addr

--- fayawall/src/ipv4.rs
+++ fayawall/src/ipv4.rs
@@ -2,6 +2,7 @@ use std::net::Ipv4Addr;
 
 use tracing::warn;
 
+#[derive(Debug, PartialEq)]
 pub struct Addr(pub Vec<Ipv4Addr>);
 
 impl Addr {
@@ -17,5 +18,39 @@ impl Addr {
                 })
                 .collect(),
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_invalid_addrs() {
+        let result = Addr::parse(&["-1.0.0.0", "256.0.0.0"]);
+        assert_eq!(result, Addr(vec![]));
+    }
+
+    #[test]
+    fn parse_no_addr() {
+        let result = Addr::parse(&[]);
+        assert_eq!(result, Addr(vec![]));
+    }
+
+    #[test]
+    fn parse_valid_addrs() {
+        let result = Addr::parse(&["0.0.0.0", "255.255.255.255"]);
+        let expected = Addr(vec![
+            Ipv4Addr::new(0, 0, 0, 0),
+            Ipv4Addr::new(255, 255, 255, 255),
+        ]);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn parse_valid_addr_and_invalid_addr() {
+        let result = Addr::parse(&["127.0.0.1", "invalid"]);
+        assert_eq!(result, Addr(vec![Ipv4Addr::new(127, 0, 0, 1)]));
     }
 }
