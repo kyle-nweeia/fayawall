21a8bbce088dd5478b627cbc6d20ec3a6adee9b9
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-09-23 21:15:21 -0600

    Define Log

--- fayawall/src/log.rs
+++ fayawall/src/log.rs
@@ -6,6 +6,8 @@ use tracing_appender::non_blocking::WorkerGuard;
 use tracing_log::LogTracer;
 use tracing_subscriber::{Registry, filter::Targets, fmt::Layer, prelude::*};
 
+pub struct Log;
+
 pub fn init() -> anyhow::Result<WorkerGuard> {
     let file_appender = tracing_appender::rolling::never(".", "fayawall.log");
     let (file_writer, guard) = tracing_appender::non_blocking(file_appender);
