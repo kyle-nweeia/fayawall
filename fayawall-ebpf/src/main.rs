3fe2d2d4a6b4a4897ee6de4a64e3ee2fc420bd2f
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-09-30 12:35:19 -0600

    Call unwrap_or on Result

--- fayawall-ebpf/src/main.rs
+++ fayawall-ebpf/src/main.rs
@@ -1,14 +1,11 @@
 #![no_main]
 #![no_std]
 
-use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_ebpf::{bindings::xdp_action::XDP_ABORTED, macros::xdp, programs::XdpContext};
 use fayawall_ebpf::xdp::try_xdp_firewall;
 use panic_halt as _;
 
 #[xdp]
 pub fn xdp_firewall(ctx: XdpContext) -> u32 {
-    match try_xdp_firewall(ctx) {
-        Ok(ret) => ret,
-        Err(_) => xdp_action::XDP_ABORTED,
-    }
+    try_xdp_firewall(ctx).unwrap_or(XDP_ABORTED)
 }
